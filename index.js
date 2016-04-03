f97365c9bbfaf763366c1f3fab669dee05e1cfcf
Nishant Rodrigues <nishantjr@gmail.com>
2016-04-03 23:23:06 +0800

    index.js: Implement basic markdown rendering.
    This doesn't enable any extra features such as html in markdown, footnotes etc.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 'use strict'
 
+const md = require('markdown-it')()
+
 const plugin = {
     name: 'markdownit',
+    render: (opts, next) => {
+        if (opts.inExtension != 'md' &&
+            opts.inExtension != 'markdown')
+           return next()
+        if (opts.outExtention != null &&
+            opts.outExtention != 'html')
+           return next()
+        opts.content = md.render(opts.content)
+        next()
+    }
+
 }
 
 module.exports = function (BasePlugin) {
